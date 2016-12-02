c6e6ce144d410de6a415937e8a8e42909037d882
Yoseph Anderson <yrganderson@gmail.com>
2016-12-02 13:07:05 -0700

    youtube video response

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
-import YoutubeApi from './youtube_api'
+import SearchBar from './components/search_bar';
+import YoutubeApi from './youtube_api';
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search';
 
-const apiKey = new YoutubeApi().key;
+let youtubeApi = new YoutubeApi();
+const apiKey = youtubeApi.key;
 
-const App = () => {
-  return(
-    <SearchBar />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: apiKey, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos }/>
+      </div>
+    )
+  }
 }
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
