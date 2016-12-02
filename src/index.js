e6f04fae9c0fb601b626df909139431f752692bc
Yoseph Anderson <yrganderson@gmail.com>
2016-12-02 11:12:05 -0700

    secure youtube api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
+import YoutubeApi from './youtube_api'
+
+const apiKey = new YoutubeApi().key;
 
 const App = () => {
   return(
